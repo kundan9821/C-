adaf6947862ad4cdcafdea446e4daec5deba457a
kundan9821 <125602381+kundan9821@users.noreply.github.com>
2023-02-17 01:00:41 +0530

    Add files via upload

---
+++ practic5e1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    for(int i=0;i<=5;i++){
+        cout<<i<<" "<<endl;
+        i++;
+        // it will print even number becz start with 0 and inrement of 2 
+    }
+    return 0;
+}
